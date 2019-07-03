c8e00d46ab7467485f39c830fe26a82fa655c276
Denis Sidorov <dsigmat@hotmail.com>
2019-07-03 09:55:30 +0600

    Add project files.

---
+++ Console16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Console16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ()
+            {
+
+            }
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
